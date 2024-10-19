bc594ad56aaf354a47d7773fbb77f6a2d26bd4d9
Nimira43 <n.rabone72@aol.com>
2024-10-19 17:39:45 +0100

    Starting to use Ant Design components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Button, Input} from 'antd'
 
 const HomePage = () => {
   return (
-    <div className='p-5'>
+    <div className='p-5 flex flex-col gap-5 w-max'>
       <h1>Medi-Care - Home Page</h1>
+      <Button type='primary' className='bg-[#ff4500]'>Submit</Button>
+      
     </div>
   )
 }
