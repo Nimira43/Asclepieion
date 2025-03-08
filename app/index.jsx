20edda4326291fabfed80b8cd8aaadefbb43026d
Nimira43 <n.rabone72@aol.com>
2025-03-08 15:03:55 +0000

    Brought in Poppins and VT323 fonts and set those up.

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,30 @@
 import { Text, View, StyleSheet } from 'react-native'
+import { useFonts } from 'expo-font'
+import AppLoading from 'expo-app-loading'
 
 const HomeScreen = () => {
+  const [fontsLoaded] = useFonts({
+    'PoppinsLight': require('../assets/fonts/Poppins-Light.ttf'),
+    'VT323': require('../assets/fonts/VT323-Regular.ttf')
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View
       style={ styles.container }
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.logoText}>Native Tasks</Text>
+      <Text
+        style={{
+          ...styles.text,
+          fontFamily: 'PoppinsLight'
+        }
+      }>
+        Task Management Application
+      </Text>
     </View>
   )
 }
@@ -15,7 +34,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  logoText: {
+    fontFamily: 'VT323', 
+    fontSize: 32,
+    color: '#ff4500',
+  },
+  text: {
+    fontSize: 18,
+  },
 })
 
 export default HomeScreen
